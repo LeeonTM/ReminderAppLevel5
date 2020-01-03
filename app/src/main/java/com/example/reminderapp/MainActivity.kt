203d8f4f9618d0e6e4c4db3eec3d34dcde873b7d
Leon Timmerman <leontimmerman123@gmail.com>
2020-01-03 16:27:02 +0100

    changes

--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -3,21 +3,17 @@ package com.example.reminderapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 const val ADD_REMINDER_REQUEST_CODE = 100
 
@@ -25,14 +21,25 @@ class MainActivity : AppCompatActivity() {
 
     private val reminders = arrayListOf<Reminder>()
     private val reminderAdapter = ReminderAdapter(reminders)
-    private lateinit var reminderRepository: ReminderRepository
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        reminderRepository = ReminderRepository(this)
+
         initViews()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+
+        viewModel.reminders.observe(this, Observer { reminders ->
+            this@MainActivity.reminders.clear()
+            this@MainActivity.reminders.addAll(reminders)
+            reminderAdapter.notifyDataSetChanged()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -64,20 +71,6 @@ class MainActivity : AppCompatActivity() {
 
         // Add swipe to delete function to remindersView
         createItemTouchHelper().attachToRecyclerView(rvReminders)
-
-        getRemindersFromDatabase()
-    }
-
-    private fun getRemindersFromDatabase() {
-        CoroutineScope(Dispatchers.Main).launch {
-            var reminders = withContext(Dispatchers.IO) {
-                reminderRepository.getAllReminders()
-            }
-
-            this@MainActivity.reminders.clear()
-            this@MainActivity.reminders.addAll(reminders)
-            reminderAdapter.notifyDataSetChanged()
-        }
     }
 
     private fun createItemTouchHelper(): ItemTouchHelper {
@@ -90,12 +83,7 @@ class MainActivity : AppCompatActivity() {
                 val position = viewHolder.adapterPosition
                 var reminderToDelete = reminders[position]
 
-                CoroutineScope(Dispatchers.Main).launch {
-                    withContext(Dispatchers.IO) {
-                        reminderRepository.deleteReminder(reminderToDelete)
-                    }
-                    getRemindersFromDatabase()
-                }
+                viewModel.deleteReminder(reminderToDelete)
             }
         }
 
@@ -112,13 +100,7 @@ class MainActivity : AppCompatActivity() {
             when (requestCode) {
                 ADD_REMINDER_REQUEST_CODE -> {
                     val reminder = data!!.getParcelableExtra<Reminder>(EXTRA_REMINDER)
-
-                    CoroutineScope(Dispatchers.Main).launch {
-                        withContext(Dispatchers.IO) {
-                            reminderRepository.insertReminder(reminder)
-                        }
-                        getRemindersFromDatabase()
-                    }
+                    viewModel.insertReminder(reminder)
                 }
             }
         }
--- app/src/main/java/com/example/reminderapp/ReminderRepository.kt
+++ app/src/main/java/com/example/reminderapp/ReminderRepository.kt
@@ -1,6 +1,7 @@
 package com.example.reminderapp
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 
 class ReminderRepository(context: Context) {
 
@@ -11,7 +12,7 @@ class ReminderRepository(context: Context) {
         reminderDao = remindeRoomDatabase!!.reminderDao()
     }
 
-    suspend fun getAllReminders(): List<Reminder> {
+    fun getAllReminders(): LiveData<List<Reminder>> {
         return reminderDao.getAllReminders()
     }
 
